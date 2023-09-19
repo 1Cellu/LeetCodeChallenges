bc812848a17682756d48e5ffe90dafec764bfc11
1Cellu <danielcortes1499@gmail.com>
2023-09-19 18:58:00 -0300

    Another solution to 121 Best Time to Buy and Sell Stock

--- LeetCodeChallenges/Challenges/121 Best Time to Buy and Sell Stock.cs
+++ LeetCodeChallenges/Challenges/121 Best Time to Buy and Sell Stock.cs
@@ -8,24 +8,45 @@ namespace LeetCodeChallenges.Challenges
 {
     public class _121_Best_Time_to_Buy_and_Sell_Stock
     {
+        //public int MaxProfit(int[] prices)
+        //{
+        //    if (prices.Length == 0) return 0;
+
+        //    int lower = prices[0];
+        //    int profit = 0;
+        //    for (int i = 0; i < prices.Length; i++)
+        //    {
+        //        if (prices[i] <= lower)
+        //        {
+        //            lower = prices[i];
+        //            for (int j = i + 1; j < prices.Length; j++)
+        //            {
+        //                if ((prices[j] - lower) >= profit)
+        //                {
+        //                    profit = prices[j] - lower;
+        //                }
+        //            }
+        //        }
+        //    }
+        //    return profit;
+        //}
+
         public int MaxProfit(int[] prices)
         {
             if (prices.Length == 0) return 0;
 
-            int lower = prices[0];
+            int minPrice = prices[0];
             int profit = 0;
-            for (int i = 0; i < prices.Length; i++)
+            for (int i = 1; i < prices.Length; i++)
             {
-                if (prices[i] <= lower)
+                if ((prices[i] - minPrice) > profit)
                 {
-                    lower = prices[i];
-                    for (int j = i + 1; j < prices.Length; j++)
-                    {
-                        if ((prices[j] - lower) >= profit)
-                        {
-                            profit = prices[j] - lower;
-                        }
-                    }
+                    profit = prices[i] - minPrice;
+                }
+
+                if (prices[i] < minPrice)
+                {
+                    minPrice = prices[i];
                 }
             }
             return profit;
